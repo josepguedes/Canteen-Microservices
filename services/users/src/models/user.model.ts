4831a0fbd4eaf88221803a2016fac7e3299ae7a5
xavierkooijman <xavierkooijman@gmail.com>
2026-01-18 23:15:31 +0000

    simple sql db creation

--- services/users/src/models/user.model.ts
+++ services/users/src/models/user.model.ts
@@ -1,13 +1,5 @@
-import { Pool } from "pg";
 import logger from "../utils/logger";
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+import pool from "../config/db";
 
 export const getAllUsers = async () => {
   logger.info("[UserModel] Fetching all users from database");
