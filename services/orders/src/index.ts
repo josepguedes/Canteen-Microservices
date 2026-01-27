e4285f27f82799b045f39f3d5bf7e7ef4500f1ee
Jose Pedro Guedes <josepedroguedes2112@gmail.com>
2026-01-27 18:52:47 +0000

    Implemented swagger ijn the recommendations, orders and users service

--- services/orders/src/index.ts
+++ services/orders/src/index.ts
@@ -1,17 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
 import errorHandler from "./middleware/errorHandler";
 import logger from "./utils/logger";
 import orderRoutes from "./routes/order.routes";
+import swaggerDocument from "./swagger-manual.json";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  customSiteTitle: "Orders Service API",
+  customCss: '.swagger-ui .topbar { display: none }',
+}));
 
 // Routes
 app.use("/orders", orderRoutes);
+
+app.get("/", (req, res) => {
+  /* #swagger.ignore = true */
+  res.status(200).send("Orders service is healthy");
+});
 
 const PORT = process.env.PORT || 5001;
 
@@ -20,6 +32,7 @@ app.use(errorHandler);
 app.listen(PORT, () => {
   const url = `http://localhost:${PORT}`;
   logger.info(`Orders service running on port ${PORT} - ${url}`);
+  logger.info(`Swagger documentation available at ${url}/api-docs`);
 });
 
 app.use(errorHandler);
